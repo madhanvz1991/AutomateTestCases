a0e08d20f8e14234821bae9fabb1bcb8ed46964b
madhanvz1991 <madhanswamy17@gmail.com>
2015-08-31 09:42:53 +0530

    Update AutomateTestCases.java

--- src/main/java/com/AutomateTestCases.java
+++ src/main/java/com/AutomateTestCases.java
@@ -3,19 +3,19 @@ package com;
 public class AutomateTestCases {
 
 	public int oprAdd(int a, int b){
-        return a+b;
+        return a + b;
     }
 	
 	public int oprSub(int a, int b){
-        return a-b;
+        return a - b;
     }
     
-    public double oprMul(double a, double b){
-        return a+b;
+    public int oprMul(int a, int b){
+        return a * b;
     }
     
     public String oprStr(String a, String b){
-        return a + b;
+        return b;
     }
 	
 	
